e024a80134cfe67e29d330649b14f5ccc79f5f5c
Anurag MK <anuragmk10@gmail.com>
2022-12-16 01:49:12 +0530

    edit commit

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -5,6 +5,16 @@ function NavBar() {
   return (
     <div className='navbar'>
         <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="netflix" />
+        <div className='main-nav'>
+        <nav className="123">                
+        <a href="#home">Home</a>
+        <a href="#tvShows">TV Shows</a>
+        <a href="#movies">Movies</a>
+        <a href="#originals">Originals</a>
+        <a href="#">Recently Added</a>
+        <a target="_blank" href="https://codepen.io/cb2307/full/NzaOrm">Portfolio</a>        
+      </nav>
+      </div>
         <img className='avatar' src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="avatar" />
     </div>
   )
